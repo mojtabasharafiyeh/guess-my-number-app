ab13bf6502e1cbabf32cc36849ba349919f710bc
mojtabasharafiyeh <moj.sharafiyeh@gmail.com>
2021-11-16 22:22:55 +0330

    delete previius chandes

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
 'use strict';
- document.querySelector('.message').textContent="correct number! 😜";
- document.querySelector(".score").innerHTML="adas"
- document.querySelector('.check').addEventListener('click',()=>{
-     console.log(document.querySelector('.guess').value)
