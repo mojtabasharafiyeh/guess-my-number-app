8a77e521ece2c06901851b72baac858af614bcd2
mojtabasharafiyeh <moj.sharafiyeh@gmail.com>
2021-11-16 21:58:12 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+ document.querySelector('.message').textContent="correct number! 😜";
+ document.querySelector(".score").innerHTML="adas"
+ document.querySelector('.check').addEventListener('click',()=>{
+     console.log(document.querySelector('.guess').value)
