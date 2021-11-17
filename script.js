826c18ede36fe7f8dbeca0490dec73cd19279975
mojtabasharafiyeh <moj.sharafiyeh@gmail.com>
2021-11-17 17:42:27 +0330

    implement baxic logic of the game

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+// console.log(document.querySelector('.message'));
+
+// document.querySelector('.message').textContent = 'corect nmber😜';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = +document.querySelector('.guess').value;
+  console.log(guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'put number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'correct';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'lose';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'lose';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
